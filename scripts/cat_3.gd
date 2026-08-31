ab3186e2a33204a440e3ff726f96cfe9e43115f2
Morgan McElroy <morganmcelroy@msn.com>
2026-08-31 11:13:20 -0700

    moved table target to be marker above table out of table collision

--- scripts/cat_3.gd
+++ scripts/cat_3.gd
@@ -1,33 +1,20 @@
 extends CharacterBody2D
 
 const SPEED = 120.0
-const DETECTION_RADIUS = 550.0
 
-@export var table_path: NodePath
-var table: Node2D
-var wander_target: Vector2
+@export var table_marker_path: NodePath
+var table_marker: Node2D
 
 func _ready() -> void:
-	table = get_node(table_path)
-	wander_target = global_position
+	table_marker = get_node(table_marker_path)
 
 func _physics_process(delta: float) -> void:
-	var distance_to_table = global_position.distance_to(table.global_position)
+	var distance_to_marker = global_position.distance_to(table_marker.global_position)
 
-	if distance_to_table <= DETECTION_RADIUS:
-		var direction = (table.global_position - global_position).normalized()
-		velocity = direction * SPEED
+	if distance_to_marker < 5.0:
+		velocity = Vector2.ZERO
 	else:
-		var distance_to_wander_target = global_position.distance_to(wander_target)
-		if distance_to_wander_target < 5.0:
-			velocity = Vector2.ZERO
-		else:
-			var direction = (wander_target - global_position).normalized()
-			velocity = direction * (SPEED * 0.5)
+		var direction = (table_marker.global_position - global_position).normalized()
+		velocity = direction * SPEED
+
 	move_and_slide()
-
-
-func _on_timer_timeout() -> void:
-
-	var random_offset = Vector2(randf_range(-100, 100), randf_range(-100, 100))
-	wander_target = global_position + random_offset
