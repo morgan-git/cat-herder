daa57796daa44802791f6a181864626acc311094
Morgan McElroy <morganmcelroy@msn.com>
2026-08-31 17:05:27 -0700

    add bottom wall tile image for wirld boundary

--- scripts/cat_3.gd
+++ scripts/cat_3.gd
@@ -1,36 +1,15 @@
-extends CharacterBody2D
-
-const SPEED = 120.0
-const HAPPINESS_DECAY_RATE = 20
-const HAPPINESS_RISE_RATE = 10
-const DETECTION_RADIUS = 100.0
+# cat_3.gd
+extends Cat
 
 @export var table_marker_path: NodePath
-@export var happiness_bar_path: NodePath
-
 var table_marker: Node2D
-var happiness_bar: ProgressBar
-var happiness: float
 
 func _ready() -> void:
 	table_marker = get_node(table_marker_path)
-	happiness_bar = get_node(happiness_bar_path)
+	super._ready()
 
+func check_satisfied() -> bool:
+	return true
 
-func _physics_process(delta: float) -> void:
-	var distance_to_marker = global_position.distance_to(table_marker.global_position)
-	var is_satisfied = distance_to_marker <= DETECTION_RADIUS
-	if is_satisfied:
-		
-		happiness += HAPPINESS_RISE_RATE * delta
-	else:
-		happiness -= HAPPINESS_DECAY_RATE * delta
-	if distance_to_marker < 5.0:
-		velocity = Vector2.ZERO
-	else:
-		var direction = (table_marker.global_position - global_position).normalized()
-		velocity = direction * SPEED
-	
-	happiness = clamp(happiness, 0.0, 100.0)
-	happiness_bar.value = happiness
-	move_and_slide()
+func get_satisfied_direction() -> Vector2:
+	return (table_marker.global_position - global_position).normalized()
